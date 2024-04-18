d532a64d1dcb9f1b255ca77ec98331e34e4eb1d4
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2024-04-18 11:15:57 -0600

    generate template for teleport integration
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

---
+++ teleport/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - Teleport
+
+## 1.0.0 / 2024-04-18
+
+***Added***:
+
+* Initial Release
+
---
+++ teleport/datadog_checks/teleport/__init__.py
@@ -0,0 +1,5 @@
+
+from .__about__ import __version__
+from .check import TeleportCheck
+
+__all__ = ['__version__', 'TeleportCheck']
